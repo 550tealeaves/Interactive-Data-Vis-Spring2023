38bde649ab44b97ab65fe51ad8989da22e217b30
nclaire29 <nastasia.claire89@gmail.com>
2023-02-14 11:33:23 -0500

    got the button to work but using addEventListener and document.querySelector

--- 1_3_intro_to_js/app.js
+++ 1_3_intro_to_js/app.js
@@ -1,11 +1,11 @@
 console.log('hello world');
 
-// let counter = 0
-// setInterval(() => {
-//     counter++
-//     console.log(counter)
-// }, 1000); //set to repeat interval every 1000s in Inspection but not on the html
 
-//setInterval();
+let btncounter = document.querySelector("#btncount") //document.querySelector returns 1st element matching the selecter (id btncount) and stores it in variable - btncounter
+let counter = 0 //store the value of 0 in variable - counter
 
-
+btncounter.addEventListener('click', function(){ //completing the event (clicking button) calls the function
+    counter++ //++ = shorthand for increment - i+1 - so 0 + 1 and it keeps going up
+    document.querySelector("#stats").innerHTML = counter //innerHTML displays the results on website
+    console.log(counter) //will display the count in the console
+}) //this works to have the counter increase with each click
