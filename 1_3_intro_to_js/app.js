82477be29a64f12e3ca30d78e21705c870a31e03
nclaire29 <nastasia.claire89@gmail.com>
2023-02-14 14:02:31 -0500

    Recoded it to include an onclick in the app.js

--- 1_3_intro_to_js/app.js
+++ 1_3_intro_to_js/app.js
@@ -1,11 +1,33 @@
 console.log('hello world');
 
 
-let btncounter = document.querySelector("#btncount") //document.querySelector returns 1st element matching the selecter (id btncount) and stores it in variable - btncounter
-let counter = -1 //store the value of -1 in variable - counter
 
-btncounter.addEventListener('click', function(){ //completing the event (clicking button) calls the function
-    counter++ //++ = shorthand for increment - i+1 - so -1 + 1 = 0 (1st # shown) and it keeps going up
-    document.querySelector("#stats").innerHTML = counter //innerHTML displays the results on website
-    console.log(counter) //will display the count in the console
-}) //this works to have the counter increase with each click
+let count = -1; //store the variable of -1 in count (count starts at -1 so 0 is the first shown)
+let button = document.getElementById("clickthis");
+button.onclick = function () {
+    count++; //++ = shorthand for increment - i+1 - so -1 + 1 = 0 (1st # shown) and it increases
+    button.innerHTML = "Click count: " + count; //when button clicked, it will change label to say Click count + #s of clicks
+    console.log(count) //console logs the clicks so they match the HTML display
+};
+
+
+// let counterFunction(){
+//     let counter = -1;
+//     counter++
+//     document.getElementById("count").innerHTML = counter
+// }
+
+// counterFunction(); //this does not work 
+
+
+
+
+
+// let btncounter = document.querySelector("#btncount") //document.querySelector returns 1st element matching the selecter (id btncount) and stores it in variable - btncounter
+
+// btncounter.addEventListener('click', function(){ //completing the event (clicking button) calls the function
+//     counter++ //++ = shorthand for increment - i+1 - so -1 + 1 = 0 (1st # shown) and it keeps going up
+//     document.querySelector("#stats").innerHTML = counter //innerHTML displays the results on website
+//     console.log(counter) //will display the count in the console
+// }) //this works to have the counter increase with each click
+
