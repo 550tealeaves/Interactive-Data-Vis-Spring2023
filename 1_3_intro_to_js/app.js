4d15ad7a6f3fb1e0401edb532599ef1414948f98
nclaire29 <nastasia.claire89@gmail.com>
2023-02-14 12:02:10 -0500

    Set counter to -1
    First number on display is supposed to be 0
    counter++ would be -1 + 1

--- 1_3_intro_to_js/app.js
+++ 1_3_intro_to_js/app.js
@@ -2,10 +2,10 @@ console.log('hello world');
 
 
 let btncounter = document.querySelector("#btncount") //document.querySelector returns 1st element matching the selecter (id btncount) and stores it in variable - btncounter
-let counter = 0 //store the value of 0 in variable - counter
+let counter = -1 //store the value of -1 in variable - counter
 
 btncounter.addEventListener('click', function(){ //completing the event (clicking button) calls the function
-    counter++ //++ = shorthand for increment - i+1 - so 0 + 1 and it keeps going up
+    counter++ //++ = shorthand for increment - i+1 - so -1 + 1 = 0 (1st # shown) and it keeps going up
     document.querySelector("#stats").innerHTML = counter //innerHTML displays the results on website
     console.log(counter) //will display the count in the console
 }) //this works to have the counter increase with each click
