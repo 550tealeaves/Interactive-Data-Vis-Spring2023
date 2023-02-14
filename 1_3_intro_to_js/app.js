8c0558cd904a5bb1d81034d52553b980b773b7d3
nclaire29 <nastasia.claire89@gmail.com>
2023-02-14 10:50:06 -0500

    created button but used setInterval function which is incorrect

--- 1_3_intro_to_js/app.js
+++ 1_3_intro_to_js/app.js
@@ -1,1 +1,11 @@
 console.log('hello world');
+
+// let counter = 0
+// setInterval(() => {
+//     counter++
+//     console.log(counter)
+// }, 1000); //set to repeat interval every 1000s in Inspection but not on the html
+
+//setInterval();
+
+
