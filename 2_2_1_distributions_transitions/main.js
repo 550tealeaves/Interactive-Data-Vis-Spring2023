25971ee94010b3fe3103cc4caa5acd8ba885f0b2
nclaire29 <nastasia.claire89@gmail.com>
2023-03-11 15:28:42 -0500

    updating class code

--- 2_2_1_distributions_transitions/main.js
+++ 2_2_1_distributions_transitions/main.js
@@ -10,3 +10,26 @@ d3.json("../data/environmentRatings.json", d3.autoType) //json parser uploads js
     .then(data => {
         console.log(data)
 })
+
+/*DEFINE SCALES*/
+//X SCALE
+const xScale = d3.scaleLinear() //scale is linear
+.domain([0, 1]) //scales must have domain/range - domain is percentage 
+.range([margin.left, width - margin.right]) //range is (60, (width-20))
+
+//Y SCALE
+const yScale = d3.scaleLinear()
+.domain([0, 100]) //score from 0-100
+.range([height - margin.bottom, margin.top]) //SVG always start at top left corner - 0 at margin-bottom, and 100 at margin-top  ((height - 60, 20))
+
+
+/*HTML ELEMENTS*/
+//append SVG
+const svg = d3.select("#container") //select for elements w/ ID container
+.append("svg") //append the element SVG to all that was selected before
+.attr("width", width) //give attribute width and pass width variable that was defined at top 
+.attr("height", height) //give attribute height and pass height variable that was defined at top
+.style("background-color", "aquamarine") //background color = style NOT attribute
+
+
+//append circles
