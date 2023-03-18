cd4baa97887ef35e94c8fa014cd0d45476351623
550tealeaves <nastasia.claire89@gmail.com>
2023-03-18 19:53:35 -0400

    positioned axes with .attr("transform", "translate)
    .attr("transform", `translate(0,${height - margin.bottom})`) //moves axes from default position at top to the bottom by 0, (height-margin.bottom)
    
    .attr("transform", `translate(${margin.left},0)`) //positions yAxis from default - moves it left by margin.left = ticks visible

--- 2_2_tutorial2/main-2.js
+++ 2_2_tutorial2/main-2.js
@@ -1,7 +1,7 @@
 const width = window.innerWidth * 0.7,
     height = window.innerHeight * 0.7,
     margin = { top: 20, bottom: 60, left: 60, right: 40 },
-    radius = 4;
+    radius = 5;
 
 
 d3.csv("..//data/catsvdogs.csv", d3.autoType).then(data => {
@@ -32,13 +32,15 @@ d3.csv("..//data/catsvdogs.csv", d3.autoType).then(data => {
     const xAxis = d3.axisBottom(xScale)
     svg.append("g")
         .attr("class", "axis") //assigns axis class
+        .attr("transform", `translate(0,${height - margin.bottom})`) //moves axes from default position at top to the bottom by 0, (height-margin.bottom)
         .call(xAxis)
 
     //Y AXIS
     const yAxis = d3.axisLeft(yScale)
     svg.append("g")
-    .attr("class", "axis")
-    .call(yAxis)
+        .attr("class", "axis")
+        .attr("transform", `translate(${margin.left},0)`) //positions yAxis from default - moves it left by margin.left = ticks visible 
+        .call(yAxis)
 
 
 
@@ -59,6 +61,7 @@ d3.csv("..//data/catsvdogs.csv", d3.autoType).then(data => {
         .text(d => d.Dog_Owning_Households_1000s + ", " + d.Cat_Owning_Households) //labels dots
         .attr("x", d => d.Dog_Owning_Households_1000s)
         .attr("y", d => d.Cat_Owning_Households)
+        .attr("font-size", 14)
         
         
         //ALTERNATIVE WAY TO WRITE .attr("cx")
