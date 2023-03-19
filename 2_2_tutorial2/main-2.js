2431f9c6529a8966ff8f580869fb0a275a3d8419
550tealeaves <nastasia.claire89@gmail.com>
2023-03-19 11:33:17 -0400

    removed duplicate files

--- 2_2_tutorial2/main-2.js
+++ 2_2_tutorial2/main-2.js
@@ -1,7 +1,7 @@
 const width = window.innerWidth * 0.9,
-    height = window.innerHeight * 0.8,
-    margin = { top: 20, bottom: 60, left: 60, right: 40 },
-    radius = 4;
+    height = window.innerHeight * 0.9,
+    margin = { top: 20, bottom: 60, left: 60, right: 70 },
+    radius = 3;
 
 
 d3.csv("..//data/catsvdogs.csv", d3.autoType).then(data => {
@@ -11,20 +11,19 @@ d3.csv("..//data/catsvdogs.csv", d3.autoType).then(data => {
 
     //X SCALE
     const xScale = d3.scaleLinear()
-        .domain([0, d3.max(data.map(d => d.Dog_Owning_Households_1000s))])
+        .domain([0, d3.max(data.map(d => d.Percentage_of_Dog_Owners))])
         .range([margin.left, width - margin.right])
 
     //Y SCALE
     const yScale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.Cat_Owning_Households)])
+        .domain([0, d3.max(data, d => d.Percentage_of_Cat_Owners)])
         .range([height - margin.bottom, margin.top])
 
     const colorScale = d3.scaleOrdinal()
-        .domain (["D", "C"])
+        .domain(["D", "C"])
         .range(["green", "brown"])
 
-    
-    
+
     /*HTML Elements */
     //CREATE SVG
     const svg = d3.select("#container")
@@ -52,18 +51,18 @@ d3.csv("..//data/catsvdogs.csv", d3.autoType).then(data => {
         .selectAll("circle")
         .data(data, d => d.Location)
         .join("circle")
-        .attr("cx", d => xScale(d.Dog_Owning_Households_1000s)) //alternative below
-        .attr("cy", d => yScale(d.Cat_Owning_Households)) //alternative below
+        .attr("cx", d => xScale(d.Percentage_of_Dog_Owners)) //alternative below
+        .attr("cy", d => yScale(d.Percentage_of_Cat_Owners)) //alternative below
         .attr("r", radius)
-        .attr("fill", d => colorScale(d.Dogs_or_Cats)) //will color the circles based on this scale
-        
+        .attr("fill", d => colorScale(d.Dogs_or_Cats))
+
     svg.selectAll("labels")
         .data(data)
         .enter()
         .append("text")
-        .text(d => d.Dog_Owning_Households_1000s + ", " + d.Cat_Owning_Households) //labels dots
-        .attr("x", d => xScale(d.Dog_Owning_Households_1000s) - margin.left/6)
-        .attr("y", d => yScale(d.Cat_Owning_Households))
+        .text(d => d.Percentage_of_Dog_Owners + ", " + d.Percentage_of_Cat_Owners) //labels dots
+        .attr("x", d => xScale(d.Percentage_of_Dog_Owners))
+        .attr("y", d => yScale(d.Percentage_of_Cat_Owners))
         .attr("font-size", 11)
 
     svg
@@ -72,25 +71,25 @@ d3.csv("..//data/catsvdogs.csv", d3.autoType).then(data => {
         .attr("x", width / 2)
         .attr("y", height / 25) //higher the denominator, higher the text moves up pg
         .attr("text-anchor", "middle")
-        .text("Dog vs Cat Ownership By State")
+        .text("Percentage Dog vs Cat Ownership By State")
         .style("font-size", "18px")
         .style("text-decoration", "underline")
         .attr("fill", "darkblue")
-        
-        
-        //ALTERNATIVE WAY TO WRITE .attr("cx")
-        // .attr("cx", function(d) {
-        //     return d.Dog_Owning_Households_1000s;
-        // })
 
 
-        //ALTERNATIVE WAY TO WRITE .attr("cy")
-        // .attr("cy", function(d) {
-        //     return d.Cat_Owning_Households;
-        // })
+    //ALTERNATIVE WAY TO WRITE .attr("cx")
+    // .attr("cx", function(d) {
+    //     return d.Dog_Owning_Households_1000s;
+    // })
 
-        //ALTERNATIVE WAY TO FIND THE AREA FOR CIRCLE SIZES
-        //.attr("r", d=> Math.sqrt(height - radius)) - this makes the circles very big
+
+    //ALTERNATIVE WAY TO WRITE .attr("cy")
+    // .attr("cy", function(d) {
+    //     return d.Cat_Owning_Households;
+    // })
+
+    //ALTERNATIVE WAY TO FIND THE AREA FOR CIRCLE SIZES
+    //.attr("r", d=> Math.sqrt(height - radius)) - this makes the circles very big
 
 
 
--- 2_2_tutorial2/main.js
+++ 2_2_tutorial2/main.js
@@ -1,58 +1,95 @@
-/* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.7,
-    height = window.innerHeight * 0.7,
+const width = window.innerWidth * 0.9,
+    height = window.innerHeight * 0.8,
     margin = { top: 20, bottom: 60, left: 60, right: 40 },
-    radius = 5;
+    radius = 4;
 
-/* LOAD DATA */
-d3.csv("../data/catsvdogs.csv", d3.autoType).then(data => {
+
+d3.csv("..//data/catsvdogs.csv", d3.autoType).then(data => {
     console.log(data)
 
-    
-    /* SCALES */
+    /**SCALES*/
 
-    // XSCALE - LINEAR, COUNT
+    //X SCALE
     const xScale = d3.scaleLinear()
-        .domain([0, d3.max(data.map(d => d.Dog_Owning_Households_1000s))]) //no errors but no #s show
+        .domain([0, d3.max(data.map(d => d.Dog_Owning_Households_1000s))])
         .range([margin.left, width - margin.right])
 
-    // YSCALE - LINEAR, COUNT
+    //Y SCALE
     const yScale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.Cat_Owning_Households)]) //works
+        .domain([0, d3.max(data, d => d.Cat_Owning_Households)])
         .range([height - margin.bottom, margin.top])
 
     const colorScale = d3.scaleOrdinal()
-        .domain(["R", "D"])
-        .range(["red", "blue", "purple"])
+        .domain(["D", "C"]) //"d" & "c" = values in column Dogs_or_Cats
+        .range(["green", "brown"])
 
-    
-    /* HTML ELEMENTS */
 
-    // SVG
+
+    /*HTML Elements */
+    //CREATE SVG
     const svg = d3.select("#container")
         .append("svg")
         .attr("width", width)
         .attr("height", height)
 
-    // AXIS SCALES
+    //X AXIS 
     const xAxis = d3.axisBottom(xScale)
     svg.append("g")
-        .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(xAxis);
+        .attr("class", "axis") //assigns axis class
+        .attr("transform", `translate(0,${height - margin.bottom})`) //moves axes from default position at top to the bottom by 0, (height-margin.bottom)
+        .call(xAxis)
 
+    //Y AXIS
     const yAxis = d3.axisLeft(yScale)
     svg.append("g")
-        .attr("transform", `translate(${margin.left},0)`)
-        .call(yAxis);
+        .attr("class", "axis")
+        .attr("transform", `translate(${margin.left},0)`) //positions yAxis from default - moves it left by margin.left = ticks visible 
+        .call(yAxis)
 
-    // CIRCLES
+
+
     const dot = svg
         .selectAll("circle")
-        .data(data, d => d.BioID) // second argument is the unique key for that row
+        .data(data, d => d.Location)
         .join("circle")
-        .attr("cx", d => xScale(d.envScore2020))
-        .attr("cy", d => yScale(d.Percentage_of_households_with_pets)) //shows something
+        .attr("cx", d => xScale(d.Dog_Owning_Households_1000s)) //alternative below
+        .attr("cy", d => yScale(d.Cat_Owning_Households)) //alternative below
         .attr("r", radius)
-        .attr("fill", d => colorScale(d.Party))
+        .attr("fill", d => colorScale(d.Dogs_or_Cats)) //will color the circles based on this scale
 
-});
+    svg.selectAll("labels")
+        .data(data)
+        .enter()
+        .append("text")
+        .text(d => d.Dog_Owning_Households_1000s + ", " + d.Cat_Owning_Households) //labels dots
+        .attr("x", d => xScale(d.Dog_Owning_Households_1000s) - margin.left / 6)
+        .attr("y", d => yScale(d.Cat_Owning_Households))
+        .attr("font-size", 11)
+
+    svg
+        .append("text")
+        .attr("class", "title")
+        .attr("x", width / 2)
+        .attr("y", height / 25) //higher the denominator, higher the text moves up pg
+        .attr("text-anchor", "middle")
+        .text("Dog vs Cat Ownership By State")
+        .style("font-size", "18px")
+        .style("text-decoration", "underline")
+        .attr("fill", "darkblue")
+
+
+    //ALTERNATIVE WAY TO WRITE .attr("cx")
+    // .attr("cx", function(d) {
+    //     return d.Dog_Owning_Households_1000s;
+    // })
+
+
+    //ALTERNATIVE WAY TO WRITE .attr("cy")
+    // .attr("cy", function(d) {
+    //     return d.Cat_Owning_Households;
+    // })
+
+    //ALTERNATIVE WAY TO FIND THE AREA FOR CIRCLE SIZES
+    //.attr("r", d=> Math.sqrt(height - radius)) - this makes the circles very big
+
+})
