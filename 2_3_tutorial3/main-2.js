d37b45227bab6a52c759eae555dd6bae2f9c8964
550tealeaves <nastasia.claire89@gmail.com>
2023-03-21 13:57:34 -0400

    removed some files and changed a width

--- 2_3_tutorial3/main-2.js
+++ 2_3_tutorial3/main-2.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
 const width = window.innerWidth * 0.7,
     height = window.innerHeight * 0.7,
-    margin = { top: 20, left: 60, bottom: 60, right: 20 };
+    margin = { top: 20, left: 100, bottom: 60, right: 20 };
 
 /* LOAD DATA */
 d3.csv("../data/populationOverTime.csv", d => {  //parse the csv
