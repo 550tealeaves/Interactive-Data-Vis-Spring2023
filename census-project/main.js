435cc9ff2afb73f7eea4d4d1b90b132791f3f92c
550tealeaves <nastasia.claire89@gmail.com>
2023-04-24 01:51:43 -0400

    stated final project coding

---
+++ census-project/main.js
@@ -0,0 +1,9 @@
+const width = window.innerWidth * 0.9;
+height = window.innerHeight * 0.7,
+margin = { top: 20, bottom: 50, left: 60, right: 40 };
+
+let svg;
+let xScale;
+let yScale;
+let colorScale;
+
