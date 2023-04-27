85cd7660ff1fceafb85df6c305892dcef4c32506
550tealeaves <nastasia.claire89@gmail.com>
2023-04-27 11:30:46 -0400

    added total female pop by state

--- census-project/main.js
+++ census-project/main.js
@@ -1,6 +1,6 @@
 /* CONSTANTS AND GLOBALS */
 const width = window.innerWidth * .9;
-const height = 1000;
+const height = 1050;
 margin = 100;
 
 
@@ -169,10 +169,90 @@ d3.csv('../data/census.csv', d3.autoType)
             .style("font-size", "10px")
 
         
+
+        //THIRD SVG - FEMALE POPULATION BY STATE
+        const svgFemale = d3.select("#third-container")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+
+
+
+        /* SCALES */
+        // xScale
+        const xScaleFemale = d3.scaleLinear()
+            .domain([0, d3.max(data, d => d.FemalePop)]) //d3 max = function expecting an array - can pass in an accessor function
+            .range([margin, width - margin])
+
+
+        // yScale
+        const yScaleFemale = d3.scaleBand()
+            .domain(data.map(d => d.Statistics))
+            .range([height - margin, margin])
+            .paddingInner(.5)
+
+        // // colorScale
+        // const colorScale = d3.scaleOrdinal()
+        //     .domain(['running', 'chasing', 'climbing', 'eating', 'foraging'])
+        //     .range(["red", "lightblue", "green", "pink", "purple"])
+
+
+        /* AXES */
+        // xAxis
+        const xAxisFemale = d3.axisBottom(xScaleFemale)
+        svgFemale.append("g")
+            .attr("class", "axis")
+            .attr("transform", `translate(0,${height - margin})`) //works - xAxis shows at bottom
+            .call(xAxisFemale)
+
+        // yAxis
+        const yAxisFemale = d3.axisLeft(yScaleFemale)
+        svgFemale.append("g")
+            .attr("class", "axis")
+            .attr("transform", `translate(${margin},0)`) //moves the yAxis over 60px, 0 
+            .call(yAxisFemale)
+
+
+        //ADD CHART TITLE    
+        svgFemale
+            .append("text")
+            .attr("class", "title")
+            .attr("x", width / 2)
+            .attr("y", height / 16) //higher the denominator, higher the text moves up pg
+            .attr("text-anchor", "middle")
+            .text("Female Population Per State")
+            .style("font-size", "24px")
+            .style("text-decoration", "underline")
+            .attr("fill", "darkcyan")
+
+
+        // BARS
+        svgFemale.selectAll("rect")
+            .data(data)
+            .join("rect")
+            .attr("height", yScaleFemale.bandwidth()) //girth of bars 
+            .attr("width", d => xScaleFemale(d.FemalePop) - margin) //=> shorthand for function - must return a value
+            .attr("x", d => margin) //bars will start at the margin
+            .attr("y", d => yScaleFemale(d.Statistics))
+            .attr("fill", "darkred")
+
+        // AXIS LABELS
+        svgFemale.selectAll("labels")
+            .data(data)
+            .enter()
+            .append("text")
+            .text(d => d.FemalePop)
+            .attr("x", d => xScaleFemale(d.FemalePop))
+            .attr("y", d => yScaleFemale(d.Statistics) + yScaleFemale.bandwidth() / 1) //dividing by 2 puts the count the middle of the bar
+            .attr("class", "labels")
+            .style("font-size", "10px")
+
+
+
         
-        //THIRD SVG - MALE AGE BRACKET POP - STATE - not drawing anything
+        //FOURTH SVG - MALE AGE BRACKET POP - STATE - not drawing anything
         
-        const svgMaleAge = d3.select("#third-container")
+        const svgMaleAge = d3.select("#fourth-container")
             .append("svg")
             .attr("width", width)
             .attr("height", height)
