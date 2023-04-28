a97544b933d985d3e2f0e01ea2859b930ac1bb22
550tealeaves <nastasia.claire89@gmail.com>
2023-04-28 02:39:46 -0400

    created index2/main2 files - also formatted axis labels
    used .toLocaleString() - in .attr("text") of axes

--- census-project/main.js
+++ census-project/main.js
@@ -8,18 +8,16 @@ let xScale;
 let yScale;
 let yAxis;
 
-// let state = {
-//     data: [],
-// };
+let state = {
+    data: [],
+};
 
 
 /* LOAD DATA */
-Promise.all([
-    d3.csv("../data/census.csv"), 
-    d3.csv("../data/census_transposed.csv", d3.autoType),    
-]).then(([data, censustwo]) => {
+d3.csv('../data/census.csv', d3.autoType)
+    .then(data => {
         console.log("data", data)
-        console.log("censustwo", censustwo)
+
 
         /* HTML ELEMENTS */
         // APPEND SVG
@@ -93,7 +91,7 @@ Promise.all([
             .data(data)
             .enter()
             .append("text")
-            .text(d => d.TotalPopulationBySex)
+            .text(d => d.TotalPopulationBySex.toLocaleString())//returns formatted string - if # then adds commas
             .attr("x", d => xScale(d.TotalPopulationBySex))
             .attr("y", d => yScale(d.Statistics) + yScale.bandwidth() / 1) //dividing by 2 puts the count the middle of the bar
             .attr("class", "labels")
@@ -173,7 +171,7 @@ Promise.all([
             .data(data)
             .enter()
             .append("text")
-            .text(d => d.MalePop)
+            .text(d => d.MalePop.toLocaleString())
             .attr("x", d => xScaleMale(d.MalePop))
             .attr("y", d => yScaleMale(d.Statistics) + yScaleMale.bandwidth() / 1) //dividing by 2 puts the count the middle of the bar
             .attr("class", "labels")
@@ -252,92 +250,10 @@ Promise.all([
             .data(data)
             .enter()
             .append("text")
-            .text(d => d.FemalePop)
+            .text(d => d.FemalePop.toLocaleString())
             .attr("x", d => xScaleFemale(d.FemalePop))
             .attr("y", d => yScaleFemale(d.Statistics) + yScaleFemale.bandwidth() / 1) //dividing by 2 puts the count the middle of the bar
             .attr("class", "labels")
-            .style("font-size", "10px")
-
-
-
+            .style("font-size", "10px")    
         
-        //FOURTH SVG - MALE AGE BRACKET POP - STATE - not drawing anything
-        
-        const svgMaleAge = d3.select("#fourth-container")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height)
-
-
-
-        /* SCALES */
-        // xScale
-        const xScaleMaleAge = d3.scaleLinear()
-            .domain(data.map(d => (d.M_Under5Years, d.M_5to9Years, d.M_10to14Years, d.M_15to17Years, d.M_18to24Years, d.M_25to34Years, d.M_35to44Years, d.M_45to54Years, d.M_55to64Years, d.M_65to74Years, d.M_75to84Years, d.M_85YearsandOver))) //d3 max = function expecting an array - can pass in an accessor function
-            .range([margin, width - margin])
-            
-
-
-        // yScale
-        const yScaleMaleAge = d3.scaleBand()
-        .domain(['<5', '5-9', '10-14', '15-17', '18-24', '25-34', '35-44', '45-54', '55-64', '65-74', '75-84', '85+'])
-            .range([height - margin, margin])
-            .paddingInner(.5)
-            
-
-        // // colorScale
-        // const colorScale = d3.scaleOrdinal()
-        //     .domain(['running', 'chasing', 'climbing', 'eating', 'foraging'])
-        //     .range(["red", "lightblue", "green", "pink", "purple"])
-
-
-        /* AXES */
-        // xAxis
-        const xAxisMaleAge = d3.axisBottom(xScaleMaleAge)
-        svgMaleAge.append("g")
-            .attr("class", "axis")
-            .attr("transform", `translate(0,${height - margin})`) //works - xAxis shows at bottom
-            .call(xAxisMaleAge)
-
-        // yAxis
-        const yAxisMaleAge = d3.axisLeft(yScaleMaleAge)
-        svgMaleAge.append("g")
-            .attr("class", "axis")
-            .attr("transform", `translate(${margin},0)`) //moves the yAxis over 60px, 0 
-            .call(yAxisMaleAge)
-
-
-        //ADD CHART TITLE    
-        svgMaleAge
-            .append("text")
-            .attr("class", "title")
-            .attr("x", width / 2)
-            .attr("y", height / 16) //higher the denominator, higher the text moves up pg
-            .attr("text-anchor", "middle")
-            .text("Male Age Pop By State")
-            .style("font-size", "24px")
-            .style("text-decoration", "underline")
-            .attr("fill", "darkcyan")
-
-
-        BARS
-        svgMaleAge.selectAll("rect")
-            .data(data)
-            .join("rect")
-            .attr("height", yScaleMaleAge.bandwidth()) //girth of bars 
-            .attr("width", d => xScaleMaleAge - margin) //=> shorthand for function - must return a value
-            .attr("x", d => margin) //bars will start at the margin
-            .attr("y", d => yScaleMaleAge(['<5', '5-9', '10-14', '15-17', '18-24', '25-34', '35-44', '45-54', '55-64', '65-74', '75-84', '85+']))
-            .attr("fill", "darkred")
-
-        // AXIS LABELS
-        svgMaleAge.selectAll("labels")
-            .data(data)
-            .enter()
-            .append("text")
-            .text(d => data[0].M_Under5Years)
-            .attr("x", d => xScaleMaleAge(data[0].M_Under5Years))
-            .attr("y", d => yScaleMaleAge(['<5', '5-9', '10-14', '15-17', '18-24', '25-34', '35-44', '45-54', '55-64', '65-74', '75-84', '85+']) + yScaleMaleAge.bandwidth() / 1) //dividing by 2 puts the count the middle of the bar
-            .attr("class", "labels")
-            .style("font-size", "10px")
     })
--- census-project/main2.js
+++ census-project/main2.js
@@ -1,49 +1,101 @@
-// set the dimensions and margins of the graph
-const margin = { top: 20, right: 30, bottom: 40, left: 90 },
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+/* CONSTANTS AND GLOBALS */
+const width = window.innerWidth * .9;
+const height = 1050;
+margin = 100;
 
-// Parse the Data
-d3.csv('../data/census_transposed.csv', d3.autoType)
-    .then(data => {
-        console.log("data", data)
+let svg;
+let xScale;
+let yScale;
+let yAxis;
 
-        // append the svg object to the body of the page
-        const svg = d3.select("#container")
-            .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", `translate(${margin.left}, ${margin.top})`);
+let state = {
+    data: [],
+};
 
-    
-    // Add X axis
-    const x = d3.scaleLinear()
-        .domain([0, d3.max(data.map(d => d.FemUR_Employed))])
-        .range([0, width]);
-    svg.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
-        .style("text-anchor", "end");
+/* LOAD DATA */
+d3.csv('../data/census_transposed.csv', d3.autoType).then(data => {
+    console.log("data", data)
 
-    // Y axis
-    const y = d3.scaleBand()
-        .range([0, height])
-        .domain(data.map(d => d.Statistics))
-        .padding(.5)
-    svg.append("g")
-        .call(d3.axisLeft(y))
 
-    //Bars
-    svg.selectAll("rect")
+    //FOURTH SVG - MALE AGE BRACKET POP - STATE - not drawing anything
+
+    const svgMaleAge = d3.select("#fourth-container")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+
+
+
+    /* SCALES */
+    // xScale
+    const xScaleMaleAge = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.Statistics)]) //d3 max = function expecting an array - can pass in an accessor function
+        .range([margin, width - margin])
+
+
+
+    // yScale
+    const yScaleMaleAge = d3.scaleBand()
+        .domain(['<5', '5-9', '10-14', '15-17', '18-24', '25-34', '35-44', '45-54', '55-64', '65-74', '75-84', '85+'])
+        .range([height - margin, margin])
+        .paddingInner(.5)
+
+
+    // // colorScale
+    // const colorScale = d3.scaleOrdinal()
+    //     .domain(['running', 'chasing', 'climbing', 'eating', 'foraging'])
+    //     .range(["red", "lightblue", "green", "pink", "purple"])
+
+
+    /* AXES */
+    // xAxis
+    const xAxisMaleAge = d3.axisBottom(xScaleMaleAge)
+    svgMaleAge.append("g")
+        .attr("class", "axis")
+        .attr("transform", `translate(0,${height - margin})`) //works - xAxis shows at bottom
+        .call(xAxisMaleAge)
+
+    // yAxis
+    const yAxisMaleAge = d3.axisLeft(yScaleMaleAge)
+    svgMaleAge.append("g")
+        .attr("class", "axis")
+        .attr("transform", `translate(${margin},0)`) //moves the yAxis over 60px, 0 
+        .call(yAxisMaleAge)
+
+
+    //ADD CHART TITLE    
+    svgMaleAge
+        .append("text")
+        .attr("class", "title")
+        .attr("x", width / 2)
+        .attr("y", height / 16) //higher the denominator, higher the text moves up pg
+        .attr("text-anchor", "middle")
+        .text("Male Age Pop By State")
+        .style("font-size", "24px")
+        .style("text-decoration", "underline")
+        .attr("fill", "darkcyan")
+
+
+    //BARS
+    svgMaleAge.selectAll("rect")
         .data(data)
         .join("rect")
-        .attr("x", x(0))
-        .attr("y", d => y(d.Statistics))
-        .attr("width", d => x(d.FemUR_Employed))
-        .attr("height", y.bandwidth())
-        .attr("fill", "#69b3a2")
+        .attr("height", yScaleMaleAge.bandwidth()) //girth of bars 
+        .attr("width", d => xScaleMaleAge - margin) //=> shorthand for function - must return a value
+        .attr("x", d => margin) //bars will start at the margin
+        .attr("y", d => yScaleMaleAge(['<5', '5-9', '10-14', '15-17', '18-24', '25-34', '35-44', '45-54', '55-64', '65-74', '75-84', '85+']))
+        .attr("fill", "darkred")
+
+    // AXIS LABELS
+    svgMaleAge.selectAll("labels")
+        .data(data)
+        .enter()
+        .append("text")
+        .text(d => d.Statistics)
+        .attr("x", d => xScaleMaleAge(d.Statistics))
+        .attr("y", d => yScaleMaleAge(['<5', '5-9', '10-14', '15-17', '18-24', '25-34', '35-44', '45-54', '55-64', '65-74', '75-84', '85+']) + yScaleMaleAge.bandwidth() / 1) //dividing by 2 puts the count the middle of the bar
+        .attr("class", "labels")
+        .style("font-size", "10px")
+
 
 })
