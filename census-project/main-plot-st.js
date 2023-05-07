c70a35b3409deaefd1e3f5fbc381e05f8bd520fe
550tealeaves <nastasia.claire89@gmail.com>
2023-05-07 18:47:11 -0400

    added color code group
    var colorCode = ["M_F_ManagementBusinessandFinancialOperations", "M_F_ProfessionalandRelated", "M_F_HealthcareSupport", "M_F_ProtectiveService", "M_F_FoodPrepandServing", "M_F_BuildingandGroundsCleaningandMaintenance", "M_F_PersonalCareandService", "M_F_SalesandRelated", "M_F_OfficeandAdminSupport", "M_F_FarmingFishingandForestry", "M_F_ConstructionExtractionandMaintenance", "M_F_Production", "M_F_TranspoandMaterialMoving"]

--- census-project/main-plot-st.js
+++ census-project/main-plot-st.js
@@ -18,7 +18,7 @@ let yAxis;
 // data initialized as empty array - must load and assign it to state
 let state = {
     data: [],
-    selectedJobs: "All"
+    selectedJobs: "Management, business, finance"
 };
 
 /* LOAD DATA */
@@ -28,12 +28,44 @@ d3.csv("../data/census_occ_pct.csv", d3.autoType).then(raw_data => {
     state.data = raw_data;
     var maleJobs = ["Male_ManagementBusinessandFinancialOperations", "Male_ProfessionalandRelated", "Male_HealthcareSupport", "Male_ProtectiveService", "Male_FoodPrepandServing", "Male_BuildingandGroundsCleaningandMaintenance", "Male_PersonalCareandService", "Male_SalesandRelated", "Male_OfficeandAdminSupport", "Male_FarmingFishingandForestry", "Male_ConstructionExtractionandMaintenance", "Male_Production", "Male_TranspoandMaterialMoving"]
     var femJobs = ["Fem_ManagementBusinessandFinancialOperations", "Fem_ProfessionalandRelated", "Fem_HealthcareSupport", "Fem_ProtectiveService", "Fem_FoodPrepandServing", "Fem_BuildingandGroundsCleaningandMaintenance", "Fem_PersonalCareandService", "Fem_SalesandRelated", "Fem_OfficeandAdminSupport", "Fem_FarmingFishingandForestry", "Fem_ConstructionExtractionandMaintenance", "Fem_Production", "Fem_TranspoandMaterialMoving"]
+    var allJobs = ["Male_ManagementBusinessandFinancialOperations", "Male_ProfessionalandRelated", "Male_HealthcareSupport", "Male_ProtectiveService", "Male_FoodPrepandServing", "Male_BuildingandGroundsCleaningandMaintenance", "Male_PersonalCareandService", "Male_SalesandRelated", "Male_OfficeandAdminSupport", "Male_FarmingFishingandForestry", "Male_ConstructionExtractionandMaintenance", "Male_Production", "Male_TranspoandMaterialMoving", "Fem_ManagementBusinessandFinancialOperations", "Fem_ProfessionalandRelated", "Fem_HealthcareSupport", "Fem_ProtectiveService", "Fem_FoodPrepandServing", "Fem_BuildingandGroundsCleaningandMaintenance", "Fem_PersonalCareandService", "Fem_SalesandRelated", "Fem_OfficeandAdminSupport", "Fem_FarmingFishingandForestry", "Fem_ConstructionExtractionandMaintenance", "Fem_Production", "Fem_TranspoandMaterialMoving"]
+    var colorCode = ["M_F_ManagementBusinessandFinancialOperations", "M_F_ProfessionalandRelated", "M_F_HealthcareSupport", "M_F_ProtectiveService", "M_F_FoodPrepandServing", "M_F_BuildingandGroundsCleaningandMaintenance", "M_F_PersonalCareandService", "M_F_SalesandRelated", "M_F_OfficeandAdminSupport", "M_F_FarmingFishingandForestry", "M_F_ConstructionExtractionandMaintenance", "M_F_Production", "M_F_TranspoandMaterialMoving"]
     console.log("maleJobs", maleJobs);
     console.log("femJobs", femJobs);
+    console.log("allJobs", allJobs);
+    console.log("colorCode", colorCode);
 
-    //init();
+    init();
 });
 
 
 /* INITIALIZING FUNCTION */
 // this will be run *one time* when the data finishes loading in
+    function init(){
+        d3.select("#dropdown")
+            .selectAll("difOption")
+            .data(allJobs)
+            .enter()
+            .append("option")
+            .text(function(d) {return d;})
+            .attr("value", d => d)
+
+    //X SCALE
+    const xScale = d3.scaleLinear()
+        .domain([0, 1])
+        //.domain(d3.extent(data, d => d.Male_ManagementBusinessandFinancialOperations)) //restricting axis to the min/max of the data (to increase the spread of the plots)
+        .range([margin.left, width - margin.right])
+
+    //Y SCALE
+    const yScale = d3.scaleLinear()
+        .domain([0, 1])
+        //.domain(d3.extent(data, d => d.Fem_ManagementBusinessandFinancialOperations)) //restricting axis to the min/max of the data (to increase the spread of the plots)
+        .range([height - margin.bottom, margin.top])
+
+    const colorScale = d3.scaleOrdinal()
+        .domain(["M", "F"]) //maps to the two different values
+        .range(["purple", "orange"])
+
+
+
+}
