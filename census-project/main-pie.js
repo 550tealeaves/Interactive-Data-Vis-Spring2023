085182690bf1a1e800ab1a73bd9975b9397b9534
550tealeaves <nastasia.claire89@gmail.com>
2023-05-09 09:33:44 -0400

    deleted main-pie.js

--- census-project/main-pie.js
+++
@@ -1,9 +0,0 @@
-// set the dimensions and margins of the graph
-const width = 450, //increase width and chart moves to middle
-height = 450,
-margin = 40;
-
-d3.json("../data/census/states/all/totals.json", d3.autoType)
-    .then(data => {
-        console.log("data", data)
-    })
