93ec28ebafee53b8cf969be3b7588576eeb02b3e
550tealeaves <nastasia.claire89@gmail.com>
2023-05-09 09:33:09 -0400

    json file does not upload in pie

--- census-project/main-pie.js
+++ census-project/main-pie.js
@@ -1,49 +1,9 @@
 // set the dimensions and margins of the graph
-var width = 450 //increase width and chart moves to middle
-height = 450
-margin = 40
+const width = 450, //increase width and chart moves to middle
+height = 450,
+margin = 40;
 
-
-/* LOAD DATA */
-d3.csv("../data/census_pct.csv", d3.autoType)
+d3.json("../data/census/states/all/totals.json", d3.autoType)
     .then(data => {
         console.log("data", data)
-
     })
-
-// // The radius of the pieplot is half the width or half the height (smallest one). 
-// //I subtract a bit of margin.
-// var radius = Math.min(width, height) / 2 - margin
-
-// // append the svg object to the div called 'container'
-// var svg = d3.select("#container")
-//     .append("svg")
-//     .attr("width", width)
-//     .attr("height", height)
-//     .append("g")
-//     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-// // set the color scale
-// var color = d3.scaleOrdinal()
-//     .domain(data)
-//     .range(["#ff4949", "#38ff84", "#ff8ceb", "#ffe900", "#6fbcad"])
-
-// // Compute the position of each group on the pie:
-// var pie = d3.pie()
-//     .value(d => d.value) //replaces ({function (d) {return(d.value})}})
-// var data_ready = pie(d3.entries(data))
-
-// // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-// svg
-//     .selectAll("circle")
-//     .data(data_ready)
-//     .enter()
-//     .append("path")
-//     .attr('d', d3.arc()
-//         .innerRadius(0)
-//         .outerRadius(radius)
-//     )
-//     .attr("fill", d => (color(d.data.key))) //replaces function (d) {return (color(d.data.key))}
-//     .attr("stroke", "black")
-//     .style("stroke-width", "2px")
-//     .style("opacity", 0.7)
