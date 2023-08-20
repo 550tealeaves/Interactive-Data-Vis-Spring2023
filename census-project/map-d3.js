0bcf17d951ccf186c9c015c04ea9bd06e8b32860
550tealeaves <nastasia.claire89@gmail.com>
2023-08-20 10:19:22 -0400

    adjusted height

--- census-project/map-d3.js
+++ census-project/map-d3.js
@@ -2,7 +2,7 @@
  * CONSTANTS AND GLOBALS
  * */
 const width = window.innerWidth * 0.9;
-height = window.innerHeight * 0.7,
+height = window.innerHeight * 0.6,
     margin = { top: 20, bottom: 50, left: 60, right: 40 };
 
 
