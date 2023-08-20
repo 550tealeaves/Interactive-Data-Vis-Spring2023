a3f25a42ddd4c4431f9b72bbc77dd07264d33a3e
550tealeaves <nastasia.claire89@gmail.com>
2023-08-20 07:56:31 -0400

    got the dropdown to work but need to update the hover
    thank goodness for professor's help

--- census-project/map.js
+++ census-project/map.js
@@ -1,11 +1,7 @@
 console.log('loaded');
 
-let map = L.map('map').setView([46.0, -97.5], 3.4);
-//L. - means it comes from the Leaflet library 
-//make a map object in the div with the ID map 
-//setView - sets the starting lat/long and the zoom level
-//use geojson.io - to determine lat/lon and zoom level and insert in the setView area (can start wherever you want it to ) - data up there starts at NYC
-//bigger zoom number = more zoomed in
+//CREATE BASE MAP LAYERS
+let map = L.map('map').setView([40.7, -73.7], 5);
 
 //http://maps.stamen.com/#terrain/12/37.7706/-122.3782
 const basemap_urls = {
@@ -21,35 +17,161 @@ L.tileLayer(basemap_urls.terrain, { //will show the terrain layer
 //L.tileLayer - comes directly from Leaflet library
 //wants a URL from were to get the tiles 
 
+//Use https://stackoverflow.com/questions/9895082/javascript-populate-drop-down-list-with-array - to learn how to create dropdown and select value
+
+// Use https://stackoverflow.com/questions/1085801/get-selected-value-in-dropdown-list-using-javascript - to learn how to select value
+
+//Save value of selected option (ex: Fem_Health...from dropdown) into a variable - it saves the name of the object key
+var geojson;
 
 
-const allStates = axios("../data/usState-jobs.json").then(resp => { //brings in the map data 
+const allStates = axios('../data/usState-jobs.json').then(resp => { //brings in the map data 
     jobTitles = Object.keys(resp.data.features[0].properties) //use this to be able to select all the job titles
 
-    console.log('jobTitles', jobTitles);
-    jobValues = Object.values(resp.data.features[0].properties)
-    console.log('jobValues', jobValues)
+    jobValues = Object.values(resp.data.features[0].properties) //use this to see all the values from the key.value pairs
 
-    console.log('response', resp); //see response in console log
-    var geojson = L.geoJSON(resp.data, {
+    const profFields = {
+        'prof': {
+            'male': 'Male_ProfessionalandRelated',
+            'female': 'Fem_ProfessionalandRelated',
+            'majority': 'M_F_ProfessionalandRelated',
+        },
+        'manage': {
+            'male': 'Male_ManagementBusinessandFinancialOperations',
+            'female': 'Fem_ManagementBusinessandFinancialOperations',
+            'majority': 'M_F_ManagementBusinessandFinancialOperations',
+        },
+        'health': {
+            'male': 'Male_HealthcareSupport',
+            'female': 'Fem_HealthcareSupport',
+            'majority': 'M_F_HealthcareSupport',
+        },
+        'prot': {
+            'male': 'Male_ProtectiveService',
+            'female': 'Fem_ProtectiveService',
+            'majority': 'M_F_ProtectiveService',
+        },
+        'food': {
+            'male': 'Male_FoodPrepandServing',
+            'female': 'Fem_FoodPrepandServing',
+            'majority': 'M_F_FoodPrepandServing',
+        },
+        'build': {
+            'male': 'Male_BuildingandGroundsCleaningandMaintenance',
+            'female': 'Fem_BuildingandGroundsCleaningandMaintenance',
+            'majority': 'M_F_BuildingandGroundsCleaningandMaintenance',
+        },
+        'personal': {
+            'male': 'Male_PersonalCareandService',
+            'female': 'Fem_PersonalCareandService',
+            'majority': 'M_F_PersonalCareandService',
+        },
+        'sales': {
+            'male': 'Male_SalesandRelated',
+            'female': 'Fem_SalesandRelated',
+            'majority': 'M_F_SalesandRelated',
+        },
+        'admin': {
+            'male': 'Male_OfficeandAdminSupport',
+            'female': 'Fem_OfficeandAdminSupport',
+            'majority': 'M_F_OfficeandAdminSupport',
+        },
+        'farm': {
+            'male': 'Male_FarmingFishingandForestry',
+            'female': 'Fem_FarmingFishingandForestry',
+            'majority': 'M_F_FarmingFishingandForestry',
+        },
+        'construct': {
+            'male': 'Male_ConstructionExtractionandMaintenance',
+            'female': 'Fem_ConstructionExtractionandMaintenance',
+            'majority': 'M_F_ConstructionExtractionandMaintenance',
+        },
+        'prod': {
+            'male': 'Male_Production',
+            'female': 'Fem_Production',
+            'majority': 'M_F_Production',
+        },
+        'transp': {
+            'male': 'Male_TranspoandMaterialMoving',
+            'female': 'Fem_TranspoandMaterialMoving',
+            'majority': 'M_F_TranspoandMaterialMoving',
+        }
+    };
+    let userSelection = '';
+
+    geojson = L.geoJSON(resp.data, {
         style: function (feature) {
             return {
-                fillColor: getColorMFSales(feature),
+                fillColor: getColor(feature),
                 fillOpacity: 0.95,
                 color: 'black', //colors the borders
                 weight: 1
             }
         },
-
+        //onEachFeature - can click and display state name 
         onEachFeature: function (feature, layer) {
-            // layer.bindPopup(feature.properties.STUSPS + ': ' + '<b>' + 'F:' + '' + Math.round(feature.properties.Fem_ManagementBusinessandFinancialOperations * 100) + '%' + ' ' + ' ' + 'M:' + '' + Math.round(feature.properties.Male_ManagementBusinessandFinancialOperations * 100.0) + '%') + '</b>',
-                layer.on({
-                    mouseover: highlightFeature,
-                    mouseout: resetHighlight,
-                    click: zoomToFeature
-                });
-        } //will show state initials (stusps) F: ##% M: ##% on popup
+            // layer.bindPopup(feature.properties.STUSPS + ': ' + Math.round(feature.properties.Fem_HealthcareSupport * 100.0)  + '%' + ' <br>' ), not needed b/c highlight shows percentages
+            layer.on({
+                mouseover: highlightFeature,
+                mouseout: resetHighlight,
+                click: zoomToFeature
+            });
+        }
+
     }).addTo(map).bringToFront();
+
+    //Create event change function
+    function selectEventHandler(e) {
+        userSelection = e.target.value;
+
+        geojson.eachLayer(function (layer) {
+
+            layer.setStyle({
+                fillColor: getColor(layer.feature),
+                fillOpacity: 0.95,
+                color: 'black', //colors the borders
+                weight: 1
+            }
+            );
+
+        });
+
+    }
+    //Target the HTML that will change and add eventListener
+    document.getElementById("selectJob").addEventListener('change', selectEventHandler);
+
+
+    // // CREATE COLOR VARIABLE
+    function getColor(d) {
+
+        if (userSelection.length === 0) return '#8888';
+        //move the below 3 fields (to the hover section)
+        const fields = profFields[userSelection];
+
+        const maleValue = d.properties[fields.male];
+        console.log('males', maleValue)
+        const femaleValue = d.properties[fields.female];
+        console.log('female', femaleValue)
+
+        let majorityValue = d.properties[fields.majority];
+        console.log('majority', majorityValue)
+
+        return majorityValue == 'F' ? '#fdae6b' :
+            majorityValue == 'M' ? '#542788' :
+                '#ffffff';
+
+    }
+
+    function getStyle(feature) {
+        return {
+            fillColor: getColor(feature),
+            fillOpacity: 0.95,
+            color: 'black', //colors the borders
+            weight: 1
+        }
+    }
+
+
 
 
     function highlightFeature(e) {
@@ -79,7 +201,9 @@ const allStates = axios("../data/usState-jobs.json").then(resp => { //brings in 
     function zoomToFeature(e) {
         map.fitBounds(e.target.getBounds());
     }
+
 })
+
 
 // CONTROL THAT SHOWS STATE INFO IN HOVER
 var info = L.control();
@@ -99,199 +223,5 @@ info.update = function (props) {
 info.addTo(map);
 
 
+map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
 
-//Adding color - can find colors on https:/ / colorbrewer2.org / #type=sequential & scheme=BuGn & n=3
-
-let jobTitles = [] //create an empty array
-let userSelection = 'Fem_ManagementBusinessandFinancialOperations' //set the field string = to variable
-let userSelectionMale = 'Male_ManagementBusinessandFinancialOperations'
-let userSelectionTotal = 'Total_ManagementBusinessandFinancialOperations'
-let userSelectionMFBus = 'M_F_ManagementBusinessandFinancialOperations'
-let userSelectionMFProf = 'M_F_ProfessionalandRelated'
-let userSelectionMFHealth = 'M_F_HealthcareSupport'
-let userSelectionMFProt = 'M_F_ProtectiveService'
-let userSelectionMFFood = 'M_F_FoodPrepandServing'
-let userSelectionMFBuild = 'M_F_BuildingandGroundsCleaningandMaintenance'
-let userSelectionMFPers = 'M_F_PersonalCareandService'
-let userSelectionMFSales = 'M_F_SalesandRelated'
-let userSelectionMFOffice = 'M_F_OfficeandAdminSupport'
-let userSelectionMFFarm = 'M_F_FarmingFishingandForestry'
-let userSelectionMFCon = 'M_F_ConstructionExtractionandMaintenance'
-let userSelectionMFProd = 'M_F_Production'
-let userSelectionMFTransp = 'M_F_TranspoandMaterialMoving'
-
-
-
-function getColor(d) {
-    console.log('d', d)
-    let dataValue = d.properties[userSelection]
-    //let dataValue = d.properties['Fem_HealthcareSupport'] //will go into properties (object) and access the field Fem_Health... "d" = feature
-    //Create new variable - userSelection to replace string = d.properties[userSelection]
-    return dataValue > 0.396 ? '#67000d' :
-        dataValue > 0.352 ? '#a50f15' :
-            dataValue > 0.308 ? '#cb181d' :
-                dataValue > 0.264 ? '#ef3b2c' :
-                    dataValue > 0.22 ? '#fb6a4a' :
-                        dataValue > 0.176 ? '#fc9272' :
-                            dataValue > 0.132 ? '#fcbba1' :
-                                dataValue > 0.08 ? '#fee0d2' :
-                                    dataValue > 0.044 ? '#fff5f0' :
-                                        '#ffffcc';
-} //change the value in dataValue > value ? b/c the fields in properties are in decimals - 0-1
-
-
-function getColorMale(d) {
-    let dataValue = d.properties[userSelectionMale]
-    return dataValue > 0.396 ? '#800026' :
-        dataValue > 0.352 ? '#bd0026' :
-            dataValue > 0.308 ? '#e31a1c' :
-                dataValue > 0.264 ? '#fc4e2a' :
-                    dataValue > 0.22 ? '#fd8d3c' :
-                        dataValue > 0.176 ? '#feb24c' :
-                            dataValue > 0.132 ? '#fed976' :
-                                dataValue > 0.08 ? '#ffeda0' :
-                                    dataValue > 0.044 ? '#ffffcc' :
-                                        '#ffffff';
-} 
-
-function getColorTotal(d) {
-    let dataValue = d.properties[userSelectionTotal]
-    return dataValue > 0.396 ? '#081d58' :
-        dataValue > 0.352 ? '#253494' :
-            dataValue > 0.308 ? '#225ea8' :
-                dataValue > 0.264 ? '#1d91c0' :
-                    dataValue > 0.22 ? '#41b6c4' :
-                        dataValue > 0.176 ? '#7fcdbb' :
-                            dataValue > 0.132 ? '#c7e9b4' :
-                                dataValue > 0.08 ? '#edf8b1' :
-                                    dataValue > 0.044 ? '#ffffd9' :
-                                        '#ffffff';
-} 
-
-
-function getColorMFBus(d) {
-    let dataValue = d.properties[userSelectionMFBus]
-    return dataValue == 'F' ? '#fdae6b' :
-        dataValue == 'M' ? '#542788' :
-            '#ffffff';
-
-}
-
-
-function getColorMFProf(d) {
-    let dataValue = d.properties[userSelectionMFProf]
-    return dataValue == 'F' ? '#fdae6b' :
-        dataValue == 'M' ? '#542788' :
-            '#ffffff';
-
-}
-
-function getColorMFHealth(d) {
-    let dataValue = d.properties[userSelectionMFHealth]
-    return dataValue == 'F' ? '#fdae6b' :
-        dataValue == 'M' ? '#542788' :
-            '#ffffff';
-
-}
-
-
-function getColorMFProt(d) {
-    let dataValue = d.properties[userSelectionMFProt]
-    return dataValue == 'F' ? '#fdae6b' :
-        dataValue == 'M' ? '#542788' :
-            '#ffffff';
-
-}
-
-
-function getColorMFFood(d) {
-    let dataValue = d.properties[userSelectionMFFood]
-    return dataValue == 'F' ? '#fdae6b' :
-        dataValue == 'M' ? '#542788' :
-            '#ffffff';
-
-}
-
-
-function getColorMFBuild(d) {
-    let dataValue = d.properties[userSelectionMFBuild]
-    return dataValue == 'F' ? '#fdae6b' :
-        dataValue == 'M' ? '#542788' :
-            '#ffffff';
-
-}
-
-
-function getColorMFPers(d) {
-    let dataValue = d.properties[userSelectionMFPers]
-    return dataValue == 'F' ? '#fdae6b' :
-        dataValue == 'M' ? '#542788' :
-            '#ffffff';
-
-}
-
-function getColorMFSales(d) {
-    let dataValue = d.properties[userSelectionMFSales]
-    return dataValue == 'F' ? '#fdae6b' :
-        dataValue == 'M' ? '#542788' :
-            '#ffffff';
-
-}
-
-function getColorMFOffice(d) {
-    let dataValue = d.properties[userSelectionMFOffice]
-    return dataValue == 'F' ? '#fdae6b' :
-        dataValue == 'M' ? '#542788' :
-            '#ffffff';
-
-}
-
-
-function getColorMFFarm(d) {
-    let dataValue = d.properties[userSelectionMFFarm]
-    return dataValue == 'F' ? '#fdae6b' :
-        dataValue == 'M' ? '#542788' :
-            '#ffffff';
-
-}
-
-function getColorMFCon(d) {
-    let dataValue = d.properties[userSelectionMFCon]
-    return dataValue == 'F' ? '#fdae6b' :
-        dataValue == 'M' ? '#542788' :
-            '#ffffff';
-
-}
-
-function getColorMFProd(d) {
-    let dataValue = d.properties[userSelectionMFProd]
-    return dataValue == 'F' ? '#fdae6b' :
-        dataValue == 'M' ? '#542788' :
-            '#ffffff';
-
-}
-
-function getColorMFTransp(d) {
-    let dataValue = d.properties[userSelectionMFTransp]
-    return dataValue == 'F' ? '#fdae6b' :
-        dataValue == 'M' ? '#542788' :
-            '#ffffff';
-
-}
-
-
-
-//Create the dropdown menu by looping through an array
-['Management, Business, & Financial Operations', 'Professional & Related', 'Healthcare Support', 'Protective Service', 'Food Prep & Serving', 'Building & Grounds Cleaning & Maintenance', 'Personal Care & Service', 'Sales & Related', 'Office & Admin Support', 'Farming, Fishing, & Forestry', 'Construction, Extraction, & Maintenance', 'Production', 'Transportation & Moving'].forEach(function (item) {
-    const optionObj = document.createElement("option"); //loops through each item in the array and creates an option with the item inside
-    optionObj.textContent = item;
-    document.getElementById("dropdown").appendChild(optionObj); //select for the element w/ id selectJob and add the looped item in the array to dropdown
-});
-
-// var e = document.getElementById("selectJob");
-// var optionObj = e.value;
-// var text = e.options[e.selectedIndex].text;
-
-var popup = L.popup();
-
-map.attributionControl.addAttribution('Occupation data &copy; <a href="http://census.gov/">US Census Bureau</a>');
